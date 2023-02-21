92d5ae866569290e412750b3e53969e6aa2c5fa7
Mukesh Kumar <mukesh.kumar@ipetronik.com>
2023-02-21 15:11:50 +0530

    Making the script command line

--- azure_blob_upload.py
+++ azure_blob_upload.py
@@ -1,6 +1,9 @@
 """
-Dependency:
-pip install azure-storage-blob
+  Author: Mukesh Kumar
+  Description: IPE DXS Demo Azure Blob uplaod
+  Dependency: azure-storage-blob
+  To install: pip install azure-storage-blob
+  To run: $ python3 azure_blob_upload.py -h
 """
 
 import os
@@ -12,35 +15,41 @@ from azure.storage.blob import ContainerClient
 from azure.core.exceptions import ResourceNotFoundError, HttpResponseError
 from functools import partial
 import uuid
+import argparse
 
-# Container details
-#ACCOUNT_URL = "https://ipenf1.blob.core.windows.net"
-#ACCOUNT_KEY = "sp=racwdlmeop&st=2023-02-15T05:24:19Z&se=2023-03-31T13:24:19Z&sv=2021-06-08&sr=c&sig=OApjhedN0q4RusQXmlYUaNmrkxIfvyOScsMFORP%2F1ZA%3D"
-#AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=ipenf1;AccountKey=opIKkPQVXjKoRLbzYT9Gcrt3pz7P1RHtJVrhVBWuZxT2YjLbgPXMkUwlso9a579hOmmadogdbAd/+AStODQ+5g==;EndpointSuffix=core.windows.net"
 
-# ipenf1 account details :: Always use account usr and sas details to connect
-ACCOUNT_URL = "https://ipenf1.blob.core.windows.net"
-SAS_TOKEN = "?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2023-04-30T14:24:09Z&st=2023-02-20T06:24:09Z&spr=https,http&sig=4vedpBf25PG03lGQUz0Am55tFy3o7zXu%2FwClE5TdtUc%3D"
-AZURE_STORAGE_CONNECTION_STRING = "BlobEndpoint=https://ipenf1.blob.core.windows.net/;QueueEndpoint=https://ipenf1.queue.core.windows.net/;FileEndpoint=https://ipenf1.file.core.windows.net/;TableEndpoint=https://ipenf1.table.core.windows.net/;SharedAccessSignature=sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2023-04-30T14:24:09Z&st=2023-02-20T06:24:09Z&spr=https,http&sig=4vedpBf25PG03lGQUz0Am55tFy3o7zXu%2FwClE5TdtUc%3D"
-BLOB_SAS_URL = "https://ipenf1.blob.core.windows.net/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2023-04-30T14:24:09Z&st=2023-02-20T06:24:09Z&spr=https,http&sig=4vedpBf25PG03lGQUz0Am55tFy3o7zXu%2FwClE5TdtUc%3D"
+argParser = argparse.ArgumentParser(description='Azure Blob information. Dependency: azure-storage-blob')
+argParser.add_argument("-au", "--accurl",   dest="acc_url",   type=str, help="Blob service storage account URL")
+argParser.add_argument("-st", "--sastoken", dest="sas_token", type=str, help="SAS token of storage account")
+argParser.add_argument("-cn", "--containername", dest="container_name", type=str, help="Container name. It should be only in lowercase. default container name:ipecontainer1")
+argParser.add_argument("-bf", "--blobfolder", dest="blob_folder", type=str, help="Provide Local folder absolute path of blobs which need to be uploaded to container. Absolute path of single file can also be provided.")
 
-SOURCE_FILE = '/mnt/d/ipeauthmukesh.txt'
-#SOURCE_FILE = '/mnt/d/Content.pdf'
-#SOURCE_FILE = '/mnt/d/NF-1 readout Station/NF1_Readout_Station_Doc.pdf'
-#SOURCE_FILE = '/mnt/d/NF1_backEnd_ReDesign.zip'
-#SOURCE_FILE = '/mnt/d/webinterface.zip'
-#SOURCE_FILE = '/mnt/d/NF-1 readout Station/Logs/NF1Logs/grpcServer.log.1'
-#SOURCE_FILE = '/mnt/d/NF-1 readout Station/Logs/NF1Logs/readoutstationLog.log'
+args = argParser.parse_args()
 
+ACCOUNT_URL = args.acc_url
+SAS_TOKEN = args.sas_token
+CONTAINER_NAME = args.container_name
+BLOB_FOLDER = args.blob_folder
+print("********************************")
+print("ACCOUNT_URL    :",ACCOUNT_URL)
+print("SAS_TOKEN      :",SAS_TOKEN)
+print("CONTAINER_NAME :",CONTAINER_NAME)
+print("BLOB_FOLDER    :",BLOB_FOLDER)
+print("******************************** \n")
 class IpeAzure(object):
     
     def __init__(self) -> None:
         self.account_url = ACCOUNT_URL
         self.account_key = SAS_TOKEN
-        
-        self.connection_string = AZURE_STORAGE_CONNECTION_STRING
-        self.sas_url = BLOB_SAS_URL
+        self.container_name = CONTAINER_NAME
+        self.local_blob_folder = BLOB_FOLDER
     
+    def isBlobLocalFolderExist(self):
+        if os.path.exists(self.local_blob_folder):
+            return True
+        else:
+            return False
+
     def createContainer(self, containerName):
         # Instantiate a BlobServiceClient using a connection string
         # Create blobserviceclient from connection usl
@@ -57,7 +66,7 @@ class IpeAzure(object):
                 container_client.create_container()
                 print(f"{containerName} created")
             else:
-                print(f"{containerName} container exist already")
+                print(f"{containerName} container already exist, so no need to create it.")
         except ResourceNotFoundError as ex:
             print("The container does not exist: {}".format(ex))
         except HttpResponseError as ex:
@@ -78,7 +87,26 @@ class IpeAzure(object):
             print(f"container {containerName} does not exist. Nothing to delete.")
 
 
-    def uplaodBlobToContainer(self, containerName):
+    def uploadBlobToContainer(self, containerName):
+        
+        if not self.isBlobLocalFolderExist():
+            print(f"{self.local_blob_folder} not found.")
+            return
+
+        blob_name = None
+        blob_list = []
+        if not os.path.isdir(self.local_blob_folder):
+            blob_name = self.local_blob_folder
+        else:
+            try:
+                blob_list = os.listdir(self.local_blob_folder)
+                if len(blob_list) == 0:
+                    print(f"No local blobs found in the directory {self.local_blob_folder}")
+                    return
+            except NotADirectoryError:
+                print(f"The {self.local_blob_folder} is not a directory.")
+
+
         blob_service_client = BlobServiceClient(account_url=self.account_url, credential=self.account_key)
         container_client = blob_service_client.get_container_client(containerName)
         
@@ -87,19 +115,77 @@ class IpeAzure(object):
             container_client.create_container()
             print(f"{containerName} created")
         else:
-            print(f"Container already exist")
+            pass
+            #print(f"Container already exist")
 
-        blob_name = os.path.basename(SOURCE_FILE)
-        blob_client = container_client.get_blob_client(blob_name)
+        if not os.path.isdir(self.local_blob_folder):
+            blob_name = self.local_blob_folder
 
-        stat_info = os.stat(SOURCE_FILE)
+        """print(f"blob_list::{blob_list}")
+        source_file = blob_list[0]
+        source_file = os.path.join(self.local_blob_folder,source_file)
+        blob_name = os.path.basename(source_file)
+        blob_client = container_client.get_blob_client(blob_name)"""
+
+        if blob_name != None:
+            blob_name = os.path.basename(self.local_blob_folder)
+            blob_client = container_client.get_blob_client(blob_name)
+            if not blob_client.exists():
+                self.uploadBlob(blob_client, self.local_blob_folder)
+            else:
+                print(f'blob {blob_name} already exist in container')
+            return
+        
+        print(f"Local blob list::{blob_list}")
+        
+        for blob_name in blob_list:
+            blob_client = container_client.get_blob_client(blob_name)
+            source_blob_path = os.path.join(self.local_blob_folder, blob_name)
+            if blob_client.exists():
+                print(f'blob {blob_name} already exist in container')
+                continue
+            else:
+                self.uploadBlob(blob_client, source_blob_path)
+                
+                """stat_info = os.stat(source_file)
+                totalSize = stat_info.st_size
+                print(f"Total size of the file {source_file} is {totalSize}")
+
+                chunkBytes = 2*1024*1024
+                if totalSize < chunkBytes:
+                    chunkBytes = totalSize
+                
+                
+                copiedFlag = True
+                print(f"Chunk size : {chunkBytes} \n File size : {totalSize}")
+                startTime = datetime.now()
+                print(f"File Uplaod start time {startTime}")
+                uploadedData = 0
+                block_list = []
+                with open(source_file,"rb") as local_fh:
+                    while True:
+                        read_data = local_fh.read(chunkBytes)
+                        if not read_data:
+                            break
+                        chunk_id = str(uuid.uuid4())
+                        blob_client.stage_block(chunk_id, read_data)
+                        block_list.append(BlobBlock(chunk_id))
+                        uploadedData = uploadedData + len(read_data)
+                        uploadPercentage = (uploadedData * 100)/totalSize
+                        print(f"Data uploaded till now {uploadedData} Percentage =", round(uploadPercentage,2),"%")
+                try:
+                    blob_client.commit_block_list(block_list)
+                except:
+                    print(f"Error while copying.")"""
+
+    def uploadBlob(self,blob_client, source_file):
+        stat_info = os.stat(source_file)
         totalSize = stat_info.st_size
-        print(f"Total size of the file {SOURCE_FILE} is {totalSize}")
+        print(f"Total size of the file {source_file} is {totalSize}")
 
         chunkBytes = 2*1024*1024
         if totalSize < chunkBytes:
             chunkBytes = totalSize
-        
         
         copiedFlag = True
         print(f"Chunk size : {chunkBytes} \n File size : {totalSize}")
@@ -107,7 +193,7 @@ class IpeAzure(object):
         print(f"File Uplaod start time {startTime}")
         uploadedData = 0
         block_list = []
-        with open(SOURCE_FILE,"rb") as local_fh:
+        with open(source_file,"rb") as local_fh:
             while True:
                 read_data = local_fh.read(chunkBytes)
                 if not read_data:
@@ -126,44 +212,7 @@ class IpeAzure(object):
         endTime = datetime.now()
         print(f"File upload end time {endTime}")
         uploadTime = endTime - startTime
-        print(f"file {SOURCE_FILE} is uploaded successfully. Time taken to upload {uploadTime}")
-
-        """copiedFlag = True
-        print(f"Chunk size : {chunkBytes} \n File size : {totalSize}")
-        startTime = datetime.now()
-        print(f"File Uplaod start time {startTime}")
-        uploadedData = 0
-        with open(SOURCE_FILE,"rb") as local_fh:
-            for data in iter(partial(local_fh.read, chunkBytes), b''):
-                try:
-                    blob_client.upload_blob(data, overwrite=False)
-                    print("Chunk uploaded")
-                    uploadedData = uploadedData + len(data)
-                    uploadPercentage = (uploadedData * 100)/totalSize
-                    print(f"Data uploaded till now {uploadedData} Percentage =", round(uploadPercentage,2),"%")
-                except:
-                    copiedFlag = False
-                    break
-                
-            if not copiedFlag:
-                print(f"Error while copying.")
-                return False
-
-        endTime = datetime.now()
-        print(f"File upload end time {endTime}")
-        uploadTime = endTime - startTime
-        print(f"file {SOURCE_FILE} is uploaded successfully. Time taken to upload {uploadTime}")"""
-
-        """startTime = datetime.now()
-        print(f"start time {startTime}")
-        with open(SOURCE_FILE, "rb") as data:
-            blob_client.upload_blob(data, overwrite=True)
-        endTime = datetime.now()
-        print(f"end time {endTime}")
-        uploadTime = endTime - startTime
-
-        print(f"file {SOURCE_FILE} is uploaded successfully. Time taken to upload {uploadTime}")"""
-
+        print(f"file {source_file} is uploaded successfully. Time taken to upload {uploadTime}")
 
     def listBlobsInAContainer(self, containerName):
         blob_service_client = BlobServiceClient(account_url=self.account_url, credential=self.account_key)
@@ -194,21 +243,30 @@ class IpeAzure(object):
                 print(f"{containerName} does not exist")
                 retVal = False
             else:
-                print(f"{containerName} container exist already")
+                print(f"{containerName} container already exist.")
                 retVal = True
         except ResourceNotFoundError as ex:
             print("The container does not exist: {}".format(ex))
+            return False
         except HttpResponseError as ex:
-            print(f"create container failed HttpResponseError '{sys.exc_info()[0]}' message.\n")
+            print(f"Verify container exist failed HttpResponseError '{sys.exc_info()[0]}' message.\n")
+            return False
         except Exception as ex:
+            return False
             pass
         return retVal
 
 if __name__ == '__main__':
-    containerName = "ipecontainer1"
+    if CONTAINER_NAME == None:
+        CONTAINER_NAME = "ipecontainer1"
+    containerName = CONTAINER_NAME
+    containerName = containerName.lower()
     ipeAzure = IpeAzure()
-    ipeAzure.verifyIfContainerExist(containerName)
-    ipeAzure.createContainer(containerName)
-    ipeAzure.uplaodBlobToContainer(containerName=containerName)
+    if not ipeAzure.verifyIfContainerExist(containerName):
+        ipeAzure.createContainer(containerName)
+    if not ipeAzure.verifyIfContainerExist(containerName):
+        print("Either Account URL or SAS token is invalid. Please verify.")
+        exit()
+    ipeAzure.uploadBlobToContainer(containerName=containerName)
     ipeAzure.listBlobsInAContainer(containerName=containerName)
     #ipeAzure.deleteContainer(containerName)
